54c5e72c971db3d04efd68013acc9055a0ed56e0
shamworld <qianfeng@qianfengdeMac-mini-26.local>
2016-05-25 09:41:00 +0800

    1.第一次修改

---
+++ NvShen/NvShen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NvShen
+//
+//  Created by qianfeng on 16/5/25.
+//  Copyright © 2016年 徐聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic,copy)NSString *str;
+@end
+
